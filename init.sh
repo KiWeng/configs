0f077324a96d76b6104e737afb32ea6a77c9f235
Weng Zihan <wzh.k@outlook.com>
2021-03-24 20:41:50 +0800

    creating branch 'cn', for some obvious reasons

--- init.sh
+++ init.sh
@@ -38,8 +38,8 @@ fi
 ln -s ${PWD}/.zshrc ~/.zshrc
 
 # Installing some of the external zsh plugins.
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com.cnpmjs.org/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com.cnpmjs.org/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Configuring git, supposed oh-my-zsh is installed.
 
